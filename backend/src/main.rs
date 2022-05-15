7030b21be0183db5432e766b8ed0e71d4843f807
amorx1 <akshay.morx@gmail.com>
2022-05-15 14:05:10 +0100

    cleanup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,29 +1,28 @@
-use actix_web::{web, get, post, App, HttpResponse, HttpServer, Responder};
-use rust_bert::pipelines::translation::{TranslationModel, TranslationModelBuilder, TranslationConfig};
-use serde::{Deserialize, Serialize};
-use tch::Device;
-use rust_bert::pipelines::common::ModelType;
-use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
-use lingua::Language::{English, French, German, Spanish};
 use actix_cors::Cors;
-use std::sync::Mutex;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use lingua::Language::{English, French, German, Spanish};
+use lingua::{Language, LanguageDetector, LanguageDetectorBuilder};
 use log::info;
+use rust_bert::pipelines::common::ModelType;
+use rust_bert::pipelines::translation::{
+    TranslationConfig, TranslationModel, TranslationModelBuilder,
+};
+use serde::{Deserialize, Serialize};
+use std::sync::Mutex;
+use tch::Device;
 use tokio::task::spawn_blocking;
-
-// use rust_bert::pipelines::translation
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Query {
     pub query: String,
 }
 
-pub struct MyData {
+pub struct AppData {
     Model: TranslationModel,
 }
 
-// #[get("/")]
-pub async fn index(data: web::Data<Mutex<MyData>>) -> impl Responder {
-    // let data = data.lock().unwrap();
+pub async fn index(data: web::Data<Mutex<AppData>>) -> impl Responder {
+    let data = data.lock().unwrap();
     web::Bytes::from_static(b"Hello World")
 }
 
@@ -40,53 +39,61 @@ async fn infer_input(query: &String) -> String {
 async fn convert_lang(lang: String) -> rust_bert::pipelines::translation::Language {
     if lang == "English" {
         rust_bert::pipelines::translation::Language::English
-    }
-    else if lang == "Spanish" {
+    } else if lang == "Spanish" {
         rust_bert::pipelines::translation::Language::Spanish
-    }
-    else if lang == "German" {
+    } else if lang == "German" {
         rust_bert::pipelines::translation::Language::German
-    }
-    else if lang == "French" {
+    } else if lang == "French" {
         rust_bert::pipelines::translation::Language::French
-    }
-    else {
+    } else {
         rust_bert::pipelines::translation::Language::English
     }
 }
 
-// , data: web::Data<Mutex<MyData>>
-// #[post("/")]
-async fn pred_from_query(q: web::Json<Query>, data: web::Data<Mutex<MyData>>) -> HttpResponse {
+async fn pred_from_query(q: web::Json<Query>, data: web::Data<Mutex<AppData>>) -> HttpResponse {
     let data = data.lock().unwrap();
     let query = format!("{}", q.query);
 
     if query == "".to_string() {
-        return HttpResponse::Ok().body("Enter phrase")
+        return HttpResponse::Ok().body("Enter phrase");
     }
 
     if query.len() < 5 {
-        return HttpResponse::Ok().body("Enter phrase")  
+        return HttpResponse::Ok().body("Enter phrase");
     }
 
     let inferred_language = infer_input(&query).await;
     let source_language = convert_lang(inferred_language).await;
 
     let model = &data.Model;
-    let output = model.translate(&[query], source_language, rust_bert::pipelines::translation::Language::Spanish).unwrap();
+    let output = model
+        .translate(
+            &[query],
+            source_language,
+            rust_bert::pipelines::translation::Language::Spanish,
+        )
+        .unwrap();
     let res = &output[0];
 
     HttpResponse::Ok().body(res.to_string())
-    // HttpResponse::Ok().body("Ok")
-
 }
 
 pub fn get_model() -> TranslationModel {
     TranslationModelBuilder::new()
-    .with_model_type(ModelType::M2M100)
-        .with_source_languages(vec![rust_bert::pipelines::translation::Language::English, rust_bert::pipelines::translation::Language::Spanish, rust_bert::pipelines::translation::Language::French, rust_bert::pipelines::translation::Language::German])
-        .with_target_languages(vec![rust_bert::pipelines::translation::Language::Spanish, rust_bert::pipelines::translation::Language::French, rust_bert::pipelines::translation::Language::German])
-        .create_model().unwrap()
+        .with_model_type(ModelType::M2M100)
+        .with_source_languages(vec![
+            rust_bert::pipelines::translation::Language::English,
+            rust_bert::pipelines::translation::Language::Spanish,
+            rust_bert::pipelines::translation::Language::French,
+            rust_bert::pipelines::translation::Language::German,
+        ])
+        .with_target_languages(vec![
+            rust_bert::pipelines::translation::Language::Spanish,
+            rust_bert::pipelines::translation::Language::French,
+            rust_bert::pipelines::translation::Language::German,
+        ])
+        .create_model()
+        .unwrap()
     // TranslationModelBuilder::new()
     // .with_medium_model()
     // .with_source_languages(vec![rust_bert::pipelines::translation::Language::English, rust_bert::pipelines::translation::Language::Spanish, rust_bert::pipelines::translation::Language::French, rust_bert::pipelines::translation::Language::German])
@@ -97,31 +104,17 @@ pub fn get_model() -> TranslationModel {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init();
-    let model = spawn_blocking(move || {get_model()}).await;
-    let data = web::Data::new(Mutex::new(MyData{ Model: model.unwrap()}));
-    // let data = web::Data::new(Mutex::new(MyData{ Model: model }));
+    let model = spawn_blocking(move || get_model()).await;
+    let data = web::Data::new(Mutex::new(AppData {
+        Model: model.unwrap(),
+    }));
+
     HttpServer::new(move || {
         App::new()
-        .wrap(
-            Cors::permissive()
-        )
-        .app_data(data.clone())
-        .service(
-            web::resource("/")
-            .route(
-                web::get().to(index)
-            )
-        )
-        .service(
-            web::resource("/predict")
-            .route(
-                web::post().to(pred_from_query)
-            )
-        )
-        // .route("/", web::get().to(index))
-        // .service(index)
-        // .service(pred_from_query)
-        // .route("/predict", web::post().to(pred_from_query))
+            .wrap(Cors::permissive())
+            .app_data(data.clone())
+            .service(web::resource("/").route(web::get().to(index)))
+            .service(web::resource("/predict").route(web::post().to(pred_from_query)))
     })
     .bind("127.0.0.1:8081")?
     .run()
